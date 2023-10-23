6155b2e0ceede3f8d6befd5feffe20585c9394d1
nurovic <mertnurovic@gmail.com>
2023-10-23 17:19:23 +0300

    product detail design

---
+++ components/ProductDetailItem.tsx
@@ -0,0 +1,36 @@
+import {StyleSheet, Text, Image, View, ScrollView} from 'react-native';
+import React from 'react';
+
+export default function ProductDetailItem() {
+  return (
+    <View >
+      <Image
+        style={{
+          height: 400,
+          resizeMode: 'cover',
+          borderRadius: 12,
+          borderRadius: 5,
+        }}
+        source={{
+          uri: 'https://d326fntlu7tb1e.cloudfront.net/uploads/f5cae706-9e63-4a7d-9fdd-f63f34b93f37-seaside.jpeg',
+        }}
+      />
+      <View style={styles.productInfoContainer}>
+        <Text style={{fontWeight: 'bold', fontSize: 16}}>MACBOOK PRO 13'</Text>
+        <Text style={{fontWeight: 'medium', fontSize: 13, marginTop: 5}}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
+          animi eos eum hic consequatur itaque. Quos dicta quasi enim rerum.{' '}
+        </Text>
+        <Text style={{fontWeight: 'regular', fontSize: 14, marginTop: 5}}>
+          (2) Reviews{' '}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  productInfoContainer: {
+    margin: 12,
+  },
+});
--- screens/ProductDetail.tsx
+++ screens/ProductDetail.tsx
@@ -1,16 +1,56 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import React from 'react';
 import {useRoute} from '@react-navigation/native';
+import ProductDetailItem from '../components/ProductDetailItem';
 
 export default function ProductDetail() {
   const route = useRoute();
   const id = route.params;
-  console.log( id);
+  console.log(id);
   return (
-    <View>
-      <Text>ProductDetail</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <ProductDetailItem />
+        <ProductDetailItem />
+      </ScrollView>
+
+      <View style={styles.priceCard}>
+        <Text>2,23123 TL</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{color: 'white'}}>Add Order</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  priceCard: {
+    position: 'absolute',
+    flexDirection: 'row',
+    bottom: 0,
+    width: '100%',
+    height: 52,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderTopColor: 'gray',
+    borderTopWidth: 1,
+    paddingHorizontal: 12,
+    backgroundColor: '#fdfdfd',
+  },
+  container: {flex: 1},
+  button: {
+    backgroundColor: '#181cf1b0',
+    width: 110,
+    height: 38,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+});
