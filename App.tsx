3f3d3bf428f8d4bacec44ff33e1f534b1fdc53ee
nurovic <mertnurovic@gmail.com>
2023-11-08 14:46:12 +0300

    added GraphQl

--- App.tsx
+++ App.tsx
@@ -1,38 +1,19 @@
-// In App.js in a new project
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import ScreenNavigator from './navigators/ScreenNavigator';
 
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createStackNavigator} from '@react-navigation/stack';
-import TabNavigator from './navigators/TabNavigator';
-import Login from './screens/Login';
-import ProductDetail from './screens/ProductDetail';
-import Reviews from './screens/Reviews';
-import AddProducts from './screens/AddProducts';
-import OwnProducts from './screens/OwnProducts';
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/graphql',
+  cache: new InMemoryCache()
+});
 
-
-// const Stack = createStackNavigator();
-
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen
-          name="Home"
-          component={TabNavigator}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="ProductDetail" component={ProductDetail} />
-        <Stack.Screen name="Reviews" component={Reviews} />
-        <Stack.Screen name="AddProducts" component={AddProducts} />
-        <Stack.Screen name="Own Products" component={OwnProducts} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+const App = () => (
+  <ApolloProvider client={client}>
+    <ScreenNavigator />
+  </ApolloProvider>
+);
 
 export default App;
+
+const styles = StyleSheet.create({});
---
+++ navigators/ScreenNavigator.tsx
@@ -0,0 +1,35 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createStackNavigator} from '@react-navigation/stack';
+import TabNavigator from './TabNavigator';
+import Login from '../screens/Login';
+import ProductDetail from '../screens/ProductDetail';
+import Reviews from '../screens/Reviews';
+import AddProducts from '../screens/AddProducts';
+import OwnProducts from '../screens/OwnProducts';
+
+const Stack = createStackNavigator();
+
+function ScreenNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name="Home"
+          component={TabNavigator}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="ProductDetail" component={ProductDetail} />
+        <Stack.Screen name="Reviews" component={Reviews} />
+        <Stack.Screen name="AddProducts" component={AddProducts} />
+        <Stack.Screen name="Own Products" component={OwnProducts} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default ScreenNavigator;
